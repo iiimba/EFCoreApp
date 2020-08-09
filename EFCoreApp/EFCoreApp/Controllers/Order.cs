2f43c738ddea4613b90ce5fe2b11ce5f3fb41de2
Влад Мисниченко <qwerty326159487@mail.ru>
2020-08-10 00:04:06 +0300

    Added Order, OrderLine

---
+++ EFCoreApp/EFCoreApp/Controllers/Order.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace EFCoreApp.Controllers
+{
+    public class Order
+    {
+        public long Id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string Address { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public bool Shipped { get; set; }
+
+        public IEnumerable<OrderLine> Lines { get; set; }
+    }
+}
---
+++ EFCoreApp/EFCoreApp/Controllers/OrderLine.cs
@@ -0,0 +1,19 @@
+﻿using EFCore.Controllers;
+
+namespace EFCoreApp.Controllers
+{
+    public class OrderLine
+    {
+        public long Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Product Product { get; set; }
+
+        public long ProductId { get; set; }
+
+        public Order Order { get; set; }
+
+        public long OrderId { get; set; }
+    }
+}
