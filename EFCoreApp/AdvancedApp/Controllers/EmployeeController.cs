026746f54bd6f7d6293dc9f6d3b9e629088f0ede
Vlad Misnichenko <vladmi@payoneer.com>
2021-02-15 15:01:01 +0200

    Added new project AdvancedProject

---
+++ EFCoreApp/AdvancedApp/Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdvancedApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
