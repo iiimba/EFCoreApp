4cbfc196fbb93cb857f275f44c913ef4a8d6e225
Vlad Misnichenko <vladmi@payoneer.com>
2021-02-18 11:16:06 +0200

    Added NoActionDelete example

---
+++ EFCoreApp/AdvancedApp/Migrations/20210217103326_NoActionDelete.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvancedApp.Migrations
+{
+    public partial class NoActionDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SecondaryIdentity_Employees_PrimarySSN_PrimaryFirstName_PrimaryFamilyName",
+                table: "SecondaryIdentity");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SecondaryIdentity_Employees_PrimarySSN_PrimaryFirstName_PrimaryFamilyName",
+                table: "SecondaryIdentity",
+                columns: new[] { "PrimarySSN", "PrimaryFirstName", "PrimaryFamilyName" },
+                principalTable: "Employees",
+                principalColumns: new[] { "SSN", "FirstName", "FamilyName" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SecondaryIdentity_Employees_PrimarySSN_PrimaryFirstName_PrimaryFamilyName",
+                table: "SecondaryIdentity");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SecondaryIdentity_Employees_PrimarySSN_PrimaryFirstName_PrimaryFamilyName",
+                table: "SecondaryIdentity",
+                columns: new[] { "PrimarySSN", "PrimaryFirstName", "PrimaryFamilyName" },
+                principalTable: "Employees",
+                principalColumns: new[] { "SSN", "FirstName", "FamilyName" },
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
